ff36798894973d499acfc78e9b1c61033eece1a8
Brian-Momata <brianmomata@gmail.com>
2022-09-14 09:01:33 +0300

    Add hints method
    change code to dissallow duplicates because hints
    works well without duplicates

--- mastermind.rb
+++ mastermind.rb
@@ -2,7 +2,7 @@ module Text
   def intro
     puts '==========MASTERMIND=========='
     puts "A code maker sets a code of 4 colors denoted by letters. A code breaker
- has 12 tries to guess the right configuration of the code. After each guess, the
+has 12 tries to guess the right configuration of the code. After each guess, the
 code maker provides clues"
     puts '------------------------------'
   end
@@ -10,13 +10,13 @@ code maker provides clues"
   def rules
     puts '==========RULES=========='
     puts "There are 6 color options B(blue),O(orange),Y(yellow),G(green),P(purple)
-and N(navy). Duplicates are allowed. The colors W(white) and R(red) are reserved
+and N(navy). Duplicates are NOT allowed. The colors W(white) and R(red) are reserved
 for clues. R means that you have the right color in the right position and W
 means you have the right color but in wrong position"
     puts '------------------------------'
-    puts '___Clue example___'
-    puts "suppose the code is 'BBOY' and your guess is 'BYBY'. The clues will be
-Clues: 'RWW'. This means you have 1 right color in right position and 2 right
+    puts '___Clues example___'
+    puts "suppose the code is 'BGOY' and your guess is 'BYPO'. The hint will be
+HINT: 'RWW'. This means you have 1 right color in right position and 2 right
 colors but in wrong positions."
     puts '------------------------------'
   end
@@ -24,25 +24,18 @@ end
 
 class Mastermind
   include Text
-  COLORS = [
-    ['B', 'B', 'B', 'B'],
-    ['O', 'O', 'O', 'O'],
-    ['Y', 'Y', 'Y', 'Y'],
-    ['G', 'G', 'G', 'G'],
-    ['P', 'P', 'P', 'P'],
-    ['N', 'N', 'N', 'N']
-  ]
+  COLORS = ['B','O','Y','G','P','N']
   def initialize codebreaker
     intro
     rules
-    code = COLORS.flatten.sample(4)
-    game(code, codebreaker)
+    game(codebreaker)
   end
 
   private
 
-  def game(code, codebreaker)
+  def game(codebreaker)
     tries = 1
+    code = COLORS.sample(4)
     while tries <= 12
       puts "TRY: #{tries}"
       guess = codebreaker.guess
@@ -51,11 +44,22 @@ class Mastermind
         break
       elsif tries == 12 && guess != code
         puts "You're out of moves"
+        puts "The code is #{code}"
       else
       puts 'Try Again'
+      puts "CLUE: #{hint(code, guess)}"
       end
       tries += 1
     end
+  end
+
+  def hint(code, guess)
+    hint = []
+    guess.each do |col|
+      hint << 'R' if code.include?(col) && code.index(col) == guess.index(col)
+      hint << 'W' if code.include?(col) && code.index(col) != guess.index(col)
+    end
+    hint.sort.join
   end
 end
 
